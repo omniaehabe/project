48c67032415a7e51b535ffff50c98f038a87aa1e
omniaehabe <124244093+omniaehabe@users.noreply.github.com>
2023-02-07 13:08:44 +0200

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kidsschoole.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
